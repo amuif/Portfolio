78f86011fb7c720be64265035c59f0421c4d0865
amuif <amudihayat@gmail.com>
2025-05-20 20:11:15 +0300

    rewritting init

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import Hero from './components/custom/Hero';
-import NavBar from './components/custom/NavBar';
-import Projects from './components/custom/projects';
+import './App.css'
 
 function App() {
+
   return (
-    <div className="font-geist [background-image:linear-gradient(to_right,_#241931_60%,_#000000_100%)]">
-      <div className="relative h-screen ">
-        <NavBar />
-        <Hero />
-      </div>
-      <Projects />
-    </div>
-  );
+    <>
+         </>
+  )
 }
 
-export default App;
+export default App
