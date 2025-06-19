ec1288b136d7f13e6b6ec5413e572d4cb04437bb
amuif <amudihayat@gmail.com>
2025-06-19 11:58:55 +0300

    removed other projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -8,22 +8,22 @@ const projects: projectProps[] = [
     description:
       'Wavic is a Barcelona-based startup founded in 2022, offering a collaborative platform designed to streamline  production workflows.',
   },
-  {
-    id: 2,
-    name: 'Tour nest',
-    href: '',
-    image: '/images/tournest.png',
-    description:
-      'TourNest is a comprehensive travel booking platform designed to simplify the travel planning experience for both customers and administrators.',
-  },
-  {
-    id: 3,
-    name: 'Axon bot',
-    href: '',
-    //href: 'https://gym-7olg.vercel.app/',
-    image: '/images/chatbot.png',
-    description:
-      'Axion is an intelligent AI chatbot designed to engage, assist, and converse like a human.',
-  },
+  // {
+  //   id: 2,
+  //   name: 'Tour nest',
+  //   href: '',
+  //   image: '/images/tournest.png',
+  //   description:
+  //     'TourNest is a comprehensive travel booking platform designed to simplify the travel planning experience for both customers and administrators.',
+  // },
+  // {
+  //   id: 3,
+  //   name: 'Axon bot',
+  //   href: '',
+  //   //href: 'https://gym-7olg.vercel.app/',
+  //   image: '/images/chatbot.png',
+  //   description:
+  //     'Axion is an intelligent AI chatbot designed to engage, assist, and converse like a human.',
+  // },
 ];
 export default projects;
