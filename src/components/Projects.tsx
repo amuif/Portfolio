4ccf030d2676519cf6ecfcd16d95b19c6088c652
amuif <amudihayat@gmail.com>
2025-06-24 23:39:44 +0300

    fixed redendency issue

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -26,32 +26,19 @@ const Projects = () => {
           </p>
           */}
           <div className="flex flex-col px-1 gap-7">
-            <BentoTilt className="border border-white/20  mb-7 h-[300px] w-full mx-auto  !rounded-md lg:h-[55vh]">
-              <BentoCard
-                src={projects[0].image}
-                title={projects[0].name}
-                description={projects[0].description}
-                href={projects[0].href}
-              />
-            </BentoTilt>
-            <BentoTilt className="border border-white/20  mb-7 h-[300px] w-full mx-auto  overflow-hidden rounded-md lg:h-[55vh]">
-              <BentoCard
-                src={projects[1].image}
-                title={projects[1].name}
-                description={projects[1].description}
-                href={projects[1].href}
-              />
-            </BentoTilt>
-
-            <BentoTilt className="border border-white/20   mb-7 h-[300px] w-full mx-auto  overflow-hidden rounded-md lg:h-[55vh]">
-              <BentoCard
-                src={projects[1].image}
-                darkImage={projects[1].darkImage}
-                title={projects[1].name}
-                description={projects[1].description}
-                href={projects[1].href}
-              />
-            </BentoTilt>
+            {projects.map((project, index) => (
+              <div key={index}>
+                <BentoTilt className="border border-white/20  mb-7 h-[300px] w-full mx-auto  !rounded-md lg:h-[55vh]">
+                  <BentoCard
+                    src={project.image}
+                    title={project.name}
+                    description={project.description}
+                    href={project.href}
+                    darkImage={project.darkImage}
+                  />
+                </BentoTilt>
+              </div>
+            ))}
           </div>
         </div>
       </div>
