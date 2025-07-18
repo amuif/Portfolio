86204a371f921b8c2232978d3c417dd62de891f8
amuif <amudihayat@gmail.com>
2025-07-18 13:16:57 +0300

    edited url page

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -11,7 +11,7 @@ const projects: projectProps[] = [
   {
     id: 2,
     name: 'Clarity',
-    href: 'https://clarityfinance.netlify.app/',
+    href: 'https://clarity-financee.vercel.app/',
     image: '/images/clarity.svg',
     description:
       'Take charge of your financial future with crystal-clear control over your finances. Simplify budgeting and track spending with AI-powered insights.',
